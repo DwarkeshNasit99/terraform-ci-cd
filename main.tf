0e7acff79736980e5350868cb4f7a77aea2e38be
DwarkeshNasit99 <Dnasit5836@conestogac.on.ca>
2025-04-05 21:22:52 -0400

    Add Terraform CI/CD workflow

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316" # Change based on region
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Terraform-CICD-Instance"
+  }
+}
